b4a05520d729e559bb2064fd7f532a43334545ee
pedrohmenezes1 <pedro.mk.13@hotmail.com>
2021-12-13 21:41:30 -0300

    NodeJS

---
+++ Alura-cursos/Introducao-JS/datas.js
@@ -0,0 +1,28 @@
+/* const hoje = new Date();
+console.log( hoje.getUTCHours());
+
+
+const horas = new Date();
+console.log( horas.getHours() ); 
+
+const now = new Date;
+const tempo = now.getHours();
+
+
+const horas = new Date(5000000000000);
+console.log( horas ); */
+
+/*
+if( tempo < 16 ) {
+
+    const conteudo = tempo.querySelector(".container");
+
+    conteudo.style.background = "#fed02e";
+
+ } else {
+
+    const conteudo = tempo.querySelector(".container");
+
+    conteudo.style.background = "#1000ff";
+}
+*/
